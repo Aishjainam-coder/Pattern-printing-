2340a10a2fbc8e1a4187c1e8861d55d8c5ea8bc6
Aishwarya Jain <86430623+Aishjainam-coder@users.noreply.github.com>
2024-05-30 18:43:50 +0530

    Create hollow pyramid.cpp

---
+++ hollow pyramid.cpp
@@ -0,0 +1,29 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main(){
+    int n, p;
+    cin>> n>>p;
+    for(int i=0;i<n;i++){
+        
+        for(int first=n -(i+2) ;first>=0;first--){
+            cout<<" ";
+        }
+        for(int  sec=0;sec<2*i+1;sec++){
+             if( sec>0&&sec<2*i&&i<n-1){
+            cout<<" ";}
+        //sec k alag se no of points hein like 0 , 01, 012, 
+        //
+            else{
+            cout<<"*";}}
+            
+        for(int  th=0;th<=n  ;th++){
+            cout<<" ";}
+            cout<<endl;}}
+        
+        
+
+
+    
+
+     
